b79c4a0f652b104cd843583ab510102aa7b34f1f
himanmen Git <study.himanmen@gmail.com>
2018-02-19 11:43:49 +0900

    song_search에서 검색 및 결과출력 구현

--- melon/song/views.py
+++ melon/song/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from .models import Song
@@ -23,11 +24,29 @@ def song_search(request):
         form 배치후 method는 POST, {% csrf_token %}배치
     3. 이 함수에서 return render(...)
         * 아직 context는 사용하지 않음
+
+    - 검색 구현
+    1. input의 name을 keyword로 지정
+    2. 이 함수를 request.method 가 'GET'일 때와 'POST"일 때로 분기
+    3. request.method가 'POST'일 때
+        request.POST dict의 'name'키에 해당하는 값을
+        HttpResponse로 출력
+    4. request.method가 'GET' 일 떄 이전에 하던 템플릿 출력을 유지
+
+    - Query filter로 검색하기
+    1. keyword가 자신의 'title'에 포함되는 Song쿼리 셋 생성
+    2. 위 쿼리셋을 'songs'변수에 할당
+    3. context dict를 만들고 'songs'키에 songs변수를 할당
+    4. render의 3번쨰 인수로 context를 전달
+    5. template에 전달된 'songs'를 출력
+
     :param request:
     :return:
     """
-    context = {
-
-    }
-
+    context = dict()
+    if request.method == 'POST':
+        keyword = request.POST['keyword'].strip()
+        if keyword:
+            songs = Song.objects.filter(title__contains=keyword)
+            context['songs'] = songs
     return render(request, 'song/song_search.html', context)
