61430095283d70f4b954d703eef1e3671cae3e7d
himanmen Git <study.himanmen@gmail.com>
2018-02-26 16:50:42 +0900

    artist_detail에서 like_toggle시 원래 페이지로 갈 수 있도록 next-path값 추가

---
+++ app/artist/views/artist/detail.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, get_object_or_404
+
+from artist.models import Artist
+
+__all__ = (
+    'artist_detail',
+)
+
+
+def artist_detail(request, artist_pk):
+    context = {
+        'artist': get_object_or_404(Artist, pk=artist_pk)
+    }
+    return render(request, 'artist/artist_detail.html', context)
